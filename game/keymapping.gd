b4965e7c96457232bf332ace06bcbc1b580af4fe
Infinity :0 <82898183+Infinity-deGuzman@users.noreply.github.com>
2026-02-14 11:23:10 -0500

    tryna set up enviro

--- game/keymapping.gd
+++ game/keymapping.gd
@@ -1,9 +1,6 @@
 extends Node3D
 
-
-func keys():
-	if 
-	pass
+@onready var MainCam = get_parent().get_node("MainCam")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var cam_basis = MainCam.global_transform.basis
+	
+	if Input.is_key_pressed(KEY_Q):
+		print("Q pressed.")
