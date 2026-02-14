0f4841719289eb5eb9608bb5bacc92bc7a3a1604
Infinity :0 <82898183+Infinity-deGuzman@users.noreply.github.com>
2026-02-14 15:21:49 -0500

    changed to actions (input map)

--- game/Scripts/keymapping.gd
+++ game/Scripts/keymapping.gd
@@ -4,15 +4,21 @@ extends Node3D
 @onready var HighPres = $HighPressure
 @onready var LowPres = $LowPressure
 
+signal highpres_changed(position)
+signal lowpres_changed(position)
+var can_emit: bool = true
+const COOLDOWN = .2
+
 var keymaps: Array[Array] = [
-	[KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P],
-	[KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON],
-	[KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_PERIOD, KEY_SLASH]
+	['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+	['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';'],
+	['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', 'SLASH']
 	]
 	
 var state_array: Array
 var index
 var current_state
+var global_pos
 
 var x_pos: float = 0.0
 var z_pos: float = 0.0
@@ -24,6 +30,7 @@ func _ready() -> void:
 	current_state = state_array[index]
 
 func _input(event): # changing pressure
+	can_emit = true
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_SPACE:
 			if index == len(state_array) - 1:
@@ -37,18 +44,28 @@ func _input(event): # changing pressure
 			x_pos = 0.0
 			z_pos = 0.0
 			for input in row:
-				if event.is_pressed and event.keycode != input:
+				if !event.is_action_pressed(input):
 					pass
-				elif event.is_pressed and event.keycode == input:
-					print(current_state)
+				elif event.is_action_pressed(input):
+					#print(current_state)
 					x_pos = float(row.find(input)) / 10 * 10 - 4.5
-					print(x_pos)
+					#print(x_pos)
 					z_pos = float(keymaps.find(row)) / 10 * 5
-					print(keymaps.find(row))
-					current_state.global_position = Vector3(x_pos, 0, z_pos)
-					print(x_pos, z_pos)
+					#print(keymaps.find(row))
+					global_pos = Vector3(x_pos, 0, z_pos)
+					current_state.global_position = global_pos
+					if can_emit:
+						if current_state == HighPres:
+							highpres_changed.emit(global_pos)
+							print(highpres_changed)
+						else:
+							lowpres_changed.emit(global_pos)
+							print(lowpres_changed)
+						can_emit = false
+					#print(x_pos, z_pos)
 				else:
 					print("error with keymaps")
 
 func _process(delta: float) -> void:
 	var cam_basis = MainCam.global_transform.basis
+	
