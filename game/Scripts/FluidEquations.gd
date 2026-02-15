8b5d2809738740eef818142e8ff723ed70da8480
puffertron <puffertron@gmail.com>
2026-02-15 01:52:49 -0500

    horrible simulatioin

--- game/Scripts/FluidEquations.gd
+++ game/Scripts/FluidEquations.gd
@@ -54,8 +54,66 @@ func ciaranPropogation(domain, pressure, point, delta):
 	#Change in pressure for next state is based on total 'flow'
 	return [netFlow, netVel]
 
-func nvidaSolver(point, domain, velocity, pressure, delta):
+func nvidaSolver(point: FluidPoint, domain:FluidDomain, delta):
+	#advection
+	var RDX = 1.0
+	var a_pos = point.position - point.velocity * delta * RDX
+	var advect_vel = a_pos
+	#interpolation here
 	
+	
+
+	
+	#jacob??
+	#inscrutable equations
+	var iterations = 10
+	var jacob_vel = advect_vel
+	
+	#doin this in advance cuz loop
+	var neighbors = domain.get_orthogonal_neighbors(point)
+	var neighbors_vel = []
+	var neighbors_pressure = []
+	var neighbors_vel_sum = Vector3.ZERO
+	for n in neighbors:
+		if n:
+			neighbors_vel.append(n.velocity)
+			neighbors_vel_sum += n.velocity
+			neighbors_pressure.append(n.pressure)
+		else:
+			neighbors_vel.append(Vector3.ZERO)
+			neighbors_pressure.append(0)
+	
+	#divergence
+	var half_rdx = 0.5 / RDX
+	var vel_smth = ( (neighbors_vel[1].x - neighbors_vel[0].x ) + 
+					(neighbors_vel[3].y - neighbors_vel[2].y ) + 
+					(neighbors_vel[5].z - neighbors_vel[4].z ) )
+	var divergence = half_rdx * vel_smth
+	
+	for i in range(iterations):
+		var alpha = (jacob_vel * jacob_vel)/delta
+		var r_beta = Vector3(1,1,1)/(Vector3(4,4,4)+ (jacob_vel* jacob_vel) /delta)
+		#start by checking neighbors
+		# -x, x, -y, y, -z, z
+		
+		var iter = Vector3(0,0,0)
+		iter += neighbors_vel_sum
+		iter += alpha * jacob_vel
+		iter *= divergence
+		
+		jacob_vel = iter
+	
+	#force
+	#uhhhh
+	
+
+	#gradient subtraction
+	#neighbors pressure -x, x, -y, y, -z, z
+	var new_vel_g = jacob_vel - half_rdx * Vector3(neighbors_pressure[1] - neighbors_pressure[0],
+												neighbors_pressure[3] - neighbors_pressure[2],
+												neighbors_pressure[5] - neighbors_pressure[4])
+	
+	return [point.pressure, new_vel_g]
 	
 	
 
